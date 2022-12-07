5913f6a88461dd99192ce85bd25f5b54b610f05d
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2022-12-07 15:22:35 +0530

    added handlesubmit function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { UserList } from './components/UserList'
+import './App.css'
+import { Form } from './components/Form'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form />
+      <UserList />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
